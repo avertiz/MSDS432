2fdf092347dac7c82769dc332eb447c2ba1949ba
avertiz <63977920+avertiz@users.noreply.github.com>
2020-04-24 23:33:12 -0500

    Formatting

--- Module3.py
+++ Module3.py
@@ -25,7 +25,7 @@ def factorial_for_loop_function(x):
             factorial *= n
     return factorial
     
-    # Create array to store times and factorials
+# Create array to store times and factorials
 
 for_loop_array = np.empty((0,2), dtype = np.float64)
 
@@ -37,7 +37,7 @@ for number in random_array_1:
                          number = 10000)/1000
     for_loop_array = np.append(for_loop_array, np.array([[number, time]]), axis = 0)
     
-    # Create factorial calculation with recursion
+# Create factorial calculation with recursion
 
 def factorial_recursion_function(x):
   if x == 1:
@@ -45,7 +45,7 @@ def factorial_recursion_function(x):
   else:
     return x * factorial_recursion_function(x-1)
     
-    # Create array to store times and factorials
+# Create array to store times and factorials
 
 recursion_array = np.empty((0,2), dtype = np.float64)
 
